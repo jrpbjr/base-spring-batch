fc01bc84a2dc739ffa1e04b2f315aba5c288e981
jrpbjr <jrpbjr@gmail.com>
2025-02-21 18:26:40 -0300

    criando repository de paciente

---
+++ src/main/java/br/com/bln/basespringbatch/adapter/gateway/repository/origem/PacienteRepository.java
@@ -0,0 +1,16 @@
+package br.com.bln.basespringbatch.adapter.gateway.repository.origem;
+
+import br.com.bln.basespringbatch.domain.entity.origem.PacienteEntity;
+
+import br.com.bln.basespringbatch.domain.entity.origem.PessoaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PacienteRepository extends JpaRepository<PacienteEntity, Long> {
+
+
+    @Query("SELECT ps FROM PacienteEntity ps")
+    Page<PessoaEntity> buscarTodasPorId(Pageable pageable);
+}
