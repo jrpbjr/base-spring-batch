1d27003659936cd8a6590e561c3e1f8604e8af10
jrpbjr <jrpbjr@gmail.com>
2025-02-27 20:43:31 -0300

    Criando MapdeParaEnderecoExecutionListener

---
+++ src/main/java/br/com/bln/basespringbatch/domain/batchs/job/listner/executionlistener/MapdeParaEnderecoExecutionListener.java
@@ -0,0 +1,50 @@
+package br.com.bln.basespringbatch.domain.batchs.job.listner.executionlistener;
+
+import br.com.bln.basespringbatch.adapter.gateway.repository.destino.CaracteristicasDestinoRepository;
+import br.com.bln.basespringbatch.adapter.gateway.repository.destino.EnderecoDestinoRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Log4j2
+@Component
+public class MapdeParaEnderecoExecutionListener {
+    @Autowired
+    private EnderecoDestinoRepository enderecoDestinoRepository;
+
+    private Map<Long, Long> map = new ConcurrentHashMap<>();
+
+
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution) {
+        if (Objects.isNull(map)) {
+            this.map = new ConcurrentHashMap<Long, Long>();
+
+            enderecoDestinoRepository.mapDeParams()
+                    .stream()
+                    .forEach(obj -> {
+                        this.map.put((Long) obj[0], (Long) obj[1]);
+                    });
+
+            log.info("### StepExecutionListener -> Criando MapDeParaPessoaExecutionListener");
+        }
+
+    }
+
+    public Long obterNovoId(Long idDestino) {
+        return map.getOrDefault(idDestino, null);
+    }
+
+    @AfterStep
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        return stepExecution.getExitStatus();
+    }
+}
